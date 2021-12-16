d910b8e33fc7a1185b78ae8f5671ccfe427dfff4
srtejas <67109679+srtejas@users.noreply.github.com>
2021-12-16 15:24:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>PHP 2021  Test</h1>
-		<p><b>  Openshift test page </b></p>
+		<p><b>  This is an Openshift test page </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION 2 </h2> 
